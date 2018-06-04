89b804333df66902e4730fcb0c842124f2fc11fe
yoonkt200 <yoonkt200@gmail.com>
2018-06-04 14:55:47 +0900

    05 delete

--- 05-python-monitoring/monitoring/src/main.py
+++
@@ -1,8 +0,0 @@
-#from pyhive import hive
-import sys ; sys.path.append('../')
-import config
-
-
-if __name__ == '__main__':
-    test = config.RECMNDTN_MNTRNG_CONFIG['volm']['asscr']
-    print(test)
